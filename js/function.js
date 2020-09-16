3d174fd8b0df9967307622f0fcb9160cac2e5343
teeser07 <nathee200@gmail.com>
2020-09-16 14:07:45 +0700

    [Function]
    วิธีเขียนfunction

---
+++ js/function.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    return a + b
+}
+console.log(add(2, 3))
+
+
+function mul(a, b) {
+    return a * b
+}
+
+function searchCustomer(customerName) {
+
+}
+
+const add = (a,b) => {
+    return a + b
+} 
+console.log(add(2, 3))
