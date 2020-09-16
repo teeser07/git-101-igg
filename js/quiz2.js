b129af6b50985d59c8fd5d8f5e8c841818d4da10
teeser07 <nathee200@gmail.com>
2020-09-16 15:01:25 +0700

    Update quiz2.js

--- js/quiz2.js
+++ js/quiz2.js
@@ -7,3 +7,9 @@ for (let j = 1; j<= 20 ; j++) {
         console.log(j)
     }
 }
+
+let result1 = [-1, 2, 3, 56, 6, -93, 0, 4].map(el => el*2)
+console.log(result1)
+
+let result2 = [10, 13, 44, 55, 89, 0,12,1 ].filter(el => el%2 === 0)
+console.log(result2)
