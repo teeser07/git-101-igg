13bc29a4065f84ace3a50da8e58de444568e43a9
teeser07 <nathee200@gmail.com>
2020-09-16 15:36:10 +0700

    Update quiz3.js

--- js/quiz3.js
+++ js/quiz3.js
@@ -1,5 +1,16 @@
-if (x > 80,x < 101) {
-    return "A"
+const calculateGrade = (point) => {
+    let garde = null
+    if(point >= 81 && point <= 100) {
+        console.log("A")
+    } else if (point >= 71 && point <= 80){
+        console.log("B")
+    } else if (point >= 61 && point <= 70){
+        console.log("C")
+    } else if (point >= 51 && point <= 60){
+        console.log("D")
+    } else if (point >= 0 && point <= 50){
+        console.log("F")
+    } 
 }
 
-
+console.log(calculateGrade(55))
