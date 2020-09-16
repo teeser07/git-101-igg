9e92d3612f84d1ac43fa1cf2184b52dc495bfb71
teeser07 <nathee200@gmail.com>
2020-09-16 13:53:50 +0700

    Create quiz1.js

---
+++ js/quiz1.js
@@ -0,0 +1,9 @@
+let Profile = {
+    name : "Nathee",
+    lastname : "Niwongsa",
+    age : "20",
+    nickname : "Tee",
+    hobby : "Playing computer game",
+    tal : "0967976527"
+}
+console.log(Profile)
