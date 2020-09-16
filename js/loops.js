4cdfd1ba7cf3b841a740503b3469f6fe0c6210ff
teeser07 <nathee200@gmail.com>
2020-09-16 15:01:28 +0700

    Update loops.js

--- js/loops.js
+++ js/loops.js
@@ -1,11 +1,11 @@
-for (let i = 1; i<= 20 ; i+=1) {
-    console.log(i)
-}
+// for (let i = 1; i<= 20 ; i+=1) {
+//     console.log(i)
+// }
 
-let tamp = ['apple','babana','kiwi']
-tamp.forEach((element, idx) => {
-    console.log(idx, element)
-});
+// let tamp = ['apple','babana','kiwi']
+// tamp.forEach((element, idx) => {
+//     console.log(idx, element)
+// });
 
 let newValue = temp.map((element, idx) => {
     return `${idx+1}.${element}`
