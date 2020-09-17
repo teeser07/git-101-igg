b9c74f676f38efd3aabd513e7ea34c6eda0c60ba
teeser07 <nathee200@gmail.com>
2020-09-17 14:55:20 +0700

    Update App.js

--- hello-world-app/App.js
+++ hello-world-app/App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
 
 export default function App() {
+  const [ input, setInput ] = useState('')
+  const [color, setColor] = useState('red')
   return (
     <View style={styles.container}>
-      <Text>ทรายคนดีมีคนเดียว</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={{ uri: 'https://xn--12c4bmfplte6kc5ei9v.net/wp-content/uploads/2016/05/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%95%E0%B8%B9%E0%B8%99%E0%B9%82%E0%B8%94%E0%B9%80%E0%B8%A3%E0%B8%A1%E0%B9%88%E0%B8%AD%E0%B8%99-1.jpg' }} 
+        style={styles.logo}
+      />
+      <Text style={styles.title}>Hello World</Text>
+      <TextInput 
+        value={input} 
+        onChangeText={(text) => {setInput(text) }}
+        placeholder="Input here" 
+        style={styles.input}
+      />
+      <Text style={{fontSize:18, marginTop:20, color: color }}>{input}</Text>
+      <View style={styles.ButtonLayout}>
+        <Button title="Red" color="red" onPress={() => {setColor('red')}} />
+        <Button title="Blue" color="blue" onPress={() => {setColor('blur')}} />
+        <Button title="Green" color="green" onPress={() => {setColor('green')}} />
+      </View>
     </View>
   );
 }
@@ -17,5 +34,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 30,
   },
+  logo: {
+    width:150,
+    height:150,
+  },
+  title: {
+    fontSize:24,
+    fontWeight: 'bold',
+    marginTop: 28,
+  },
+  input: {
+    height:40,
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    borderEndWidth: 1,
+    width: '100%',
+    marginTop:20,
+  },
+  ButtonLayout: {
+    flexDirection: 'row',
+    marginTop:20,
+  }
 });
