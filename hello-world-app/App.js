45bb2efb9b5f3e36f908e69d03dbe2f7a816dfd5
teeser07 <nathee200@gmail.com>
2020-09-17 15:15:07 +0700

    Update App.js

--- hello-world-app/App.js
+++ hello-world-app/App.js
@@ -1,29 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
-
+import React , { useState } from 'react';
+import { StyleSheet, Text, TextInput, View ,Button,Image } from 'react-native';
+import MyButton from './componensts/MyButton.js'
 export default function App() {
-  const [ input, setInput ] = useState('')
-  const [color, setColor] = useState('red')
+  const [ input , setInput ] = useState('')
+  const [ color , SetColor ] =useState('red')
   return (
     <View style={styles.container}>
-      <Image 
-        source={{ uri: 'https://xn--12c4bmfplte6kc5ei9v.net/wp-content/uploads/2016/05/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%95%E0%B8%B9%E0%B8%99%E0%B9%82%E0%B8%94%E0%B9%80%E0%B8%A3%E0%B8%A1%E0%B9%88%E0%B8%AD%E0%B8%99-1.jpg' }} 
-        style={styles.logo}
-      />
-      <Text style={styles.title}>Hello World</Text>
-      <TextInput 
-        value={input} 
-        onChangeText={(text) => {setInput(text) }}
-        placeholder="Input here" 
-        style={styles.input}
-      />
-      <Text style={{fontSize:18, marginTop:20, color: color }}>{input}</Text>
-      <View style={styles.ButtonLayout}>
-        <Button title="Red" color="red" onPress={() => {setColor('red')}} />
-        <Button title="Blue" color="blue" onPress={() => {setColor('blur')}} />
-        <Button title="Green" color="green" onPress={() => {setColor('green')}} />
-      </View>
+  <Image 
+  source ={{ uri: 'https://www.igeargeek.com/_nuxt/img/835647d.png' }}
+    style={styles.logo}
+    />
+    <Text style = {styles.title} >Hello world</Text>
+    <TextInput 
+    value ={input}
+    onChangeText={(text => {setInput(text)} )}
+    placeholder ="input here" 
+    style={styles.input} 
+    />
+    <Text style={{fontSize: 18 , marginTop:20, color: color}}>
+      {input}
+      </Text>
+    <View style={styles.buttonLayout}>
+       <MyButton title="Red" color="red" onChangeColor={(color) => { SetColor(color) }} />
+       <MyButton title="Blue" color="blue" onChangeColor={(color) => { SetColor(color) }} />
+       <MyButton title="Green" color="green" onChangeColor={(color) => { SetColor(color) }} />
+    </View>
     </View>
   );
 }
@@ -34,27 +36,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 30,
   },
-  logo: {
-    width:150,
-    height:150,
+  logo:{
+    width : 150,
+    height : 150,
   },
-  title: {
-    fontSize:24,
+  title : {
+    fontSize : 24 ,
     fontWeight: 'bold',
-    marginTop: 28,
+    marginTop : 20,
   },
-  input: {
-    height:40,
-    borderColor: 'gray',
+  input : {
+    height : 40,
+    borderColor : 'gray',
     borderStyle: 'solid',
-    borderEndWidth: 1,
-    width: '100%',
-    marginTop:20,
+    borderWidth : 1,
+    width : '100%',
+    marginTop : 20,
   },
-  ButtonLayout: {
-    flexDirection: 'row',
-    marginTop:20,
+  buttonLayout:{
+    flexDirection :'row',
+    marginTop : 20,
   }
 });
