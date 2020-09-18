b3de2ea605218b4dc036a01a9145c4c2b4f8f783
teeser07 <nathee200@gmail.com>
2020-09-18 15:35:39 +0700

    Update ResumeDetail.js

--- my-resume/screens/ResumeDetail.js
+++ my-resume/screens/ResumeDetail.js
@@ -1,10 +1,47 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
+import {StyleSheet, View, Text} from 'react-native' 
 
-export default () => {
+
+export default (props) => {
+    const [resume, setResume] = useState({
+        name:'',
+        nickname:'',
+        age:'',
+        skill:'',
+    })
+
+useEffect(() => {
+    const id = props.route.params.id
+    axios.get('https://movie-api.igeargeek.com/users/profile/'+id)
+    .then((res) => {
+        console.log('res', res.data)
+        setResume(res.data)
+    })
+    .catch((error) => {
+        console.log('error', error)
+    })
+},[])
+
     return (
-        <View>
-            What
+        <View style={styles.container}>
+            <View style={styles.textLine}>
+                <Text>Full name : {resume.name}</Text>
+            </View>
+            <View style={styles.textLine}>
+                <Text>Nickname : {resume.nickname}</Text>
+            </View>
+            <View style={styles.textLine}>
+                <Text>age : {resume.age}</Text>
+            </View>
+            <View style={styles.textLine}>
+                <Text>Skill : {resume.skill}</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {padding:30, backgroundColor:'white', minHeight:'100%'},
+    textLine: { marginBottom:20 }
+})
